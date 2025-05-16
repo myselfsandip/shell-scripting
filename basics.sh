56ed4cf0e24db91d0640b7c57ec99ff8ecadd72a
myselfsandip <sandipsingha714@gmail.com>
2025-05-17 00:35:47 +0530

    Initial Commit

---
+++ basics.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#PRINT SCRIPT
+echo "Hello World!!"
+
+echo "Gooners!!!!"
+
+
+<< COMMENT 
+
+ This is a 
+Comment 
+Block
+
+COMMENT
+
+
+name="SANDIP SINGHA"
+
+echo "My Name is : $name, AND date is $(date)"
+
+echo "Enter the name: "
+
+read username
+
+echo "You Entered $username"
+
---
+++ create_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter Username: " username
+
+echo "You Entered: $username"
+
+#Create User
+sudo useradd -m $username
+
+echo "NEW USER ($username) CREATED"
+
